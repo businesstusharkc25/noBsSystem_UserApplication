7e9a3b268ae2025d001c7bc1472718994cfe7b5b
Tushar Karamchandani <tusharkaramchandani@Tushars-MacBook-Air.local>
2023-03-28 04:54:30 -0700

    helo

--- components/ui/AppHeader/styles.js
+++ components/ui/AppHeader/styles.js
@@ -18,6 +18,7 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
+    padding: 8,
   },
   headerLeft: {
     flexDirection: "row",
