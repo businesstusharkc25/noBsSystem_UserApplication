ea2a87b175ee2147841cde7016bb8abaa682dd94
tusharKc <business.tusharkc@gmail.com>
2023-03-28 10:06:18 +0530

    reverted cod

--- App.js
+++ App.js
@@ -1,16 +1,5 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { WalletConnectProvider } from "@walletconnect/react-native-dapp/dist/providers";
 import Routes from "./Routes";
 
 export default function App() {
-  return (
-    <WalletConnectProvider
-      redirectUrl={Platform.OS === "web" ? window.location.origin : `noBs://`}
-      storageOptions={{
-        asyncStorage: AsyncStorage,
-      }}
-    >
-      <Routes />
-    </WalletConnectProvider>
-  );
+  return <Routes />;
 }
