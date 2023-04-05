f07931a9ac1a8180ef3bd30360ec7dead0158ca1
Tushar Karamchandani <tusharkaramchandani@Tushars-MacBook-Air.local>
2023-04-04 22:39:39 -0700

    added changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+import "@thirdweb-dev/react-native-compat";
 
-import App from './App';
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
