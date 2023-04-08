361ed9c660c4b4a7365dde2946bca5af9f6a6f9c
Tushar Karamchandani <tusharkaramchandani@Tushars-MacBook-Air.local>
2023-04-09 00:32:56 +0530

    added changes

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { SafeAreaView, View } from "react-native";
 import { WelcomeScreen } from "../features/WelcomeScreen";
-import { ui } from "../styles";
 
 const UserWelcomeScreen = ({ navigation }) => {
   return <WelcomeScreen navigation={navigation} />;
